82e8cc1a26b1d43402f8ffa3c909da5abed4d6ff
Kello.g <68089983+KeIIog@users.noreply.github.com>
2024-06-25 01:23:47 +0900

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -272,7 +272,7 @@ class Controls:
     #  self.events.add(EventName.preEnableStandstill)
 
     if CS.gasPressed:
-      slef.longActiveUser = -1
+      self.longActiveUser = -1
       #self.events.add(EventName.gasPressedOverride)   #keiiog test
 
     if not self.CP.notCar:
